ed8dc33d3ba15b293ea0b7b8ffc9fca12447be56
Krishnanand U <krishnanandunnikrishnan@gmail.com>
2023-03-12 21:43:08 +0530

    Added hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import HomePage from "./components/screens/HomePage";
 
-function App() {
+const App = () => {
+  let headerStyles = {
+    color: "blue",
+    fontSize: 30,
+    textAlign: "center",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      
+      <div>
+        
 
+      </div>
+    </>
+  );
+};
 export default App;
